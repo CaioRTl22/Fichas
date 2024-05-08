e0c5f22d4e62db6f46b67b72c8ea8fbde93038aa
CaioRTl22 <161602339+CaioRTl22@users.noreply.github.com>
2024-05-08 12:46:40 -0300

    Delete Fichas/codigo.js

--- Fichas/codigo.js
+++
@@ -1,8 +0,0 @@
-//Visualizar Ficha
-
-async function visFicha(id){
-    //console.log(id)
-    const dados = await fetch('visualizar.php?id=' + id);
-    const resposta = await dados.json();
-    console.log(resposta);
-}
